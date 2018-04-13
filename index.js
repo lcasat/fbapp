4d6e7a385c6e0550f6e8b0b6b21458f271d5314a
Satheshkumar <lcasat@hotmail.com>
2018-04-13 09:41:44 +0530

    Added POST Method

--- index.js
+++ index.js
@@ -20,3 +20,56 @@ app.get("/webhook", function (req, res) {
     res.sendStatus(403);
   }
 });
+
+app.post("/webhook", function (req, res) {
+  logger.info('POST request has come in : ' + JSON.stringify(req.body, null, 4));
+  var reqBody = req.body;
+  var changesObj  = reqBody.entry[0].changes[0];
+  if(changesObj != null){
+  }
+  var i;
+  for(i in reqBody.entry){
+       var currSubObject = reqBody.entry[i];
+       if(currSubObject.changes !== undefined) {
+          var j
+          var currChangeObject
+          for(j in currSubObject.changes){
+              currChangeObject = currSubObject.changes[j]
+              if(currChangeObject.field === "leadgen") {
+                  handleLeadgenSync(currChangeObject); //TODO change to async later
+              }
+          }
+       }
+       else if(currSubObject.messaging !== undefined) {
+          logger.info("inside 'messaging' section")
+          handleMessageSync(req); //TODO change to async later
+       }
+  }
+  
+});
+
+var spiniBackendAPIEndpoint = "https://api.getspini.com:8443/SpinGrailsApp/external/webhook/fb/leadgen"
+var spiniAPIAccessToken = process.env.API_TOKEN
+
+function handleLeadgenSync(p_currChangeObject){
+
+    var leadgenID = p_currChangeObject.value.leadgen_id
+    logger.info("going to handle leadgen for leadgen id : " + leadgenID)
+
+    var restlerPost = restler.postJson(spiniBackendAPIEndpoint, p_currChangeObject.value,{
+        timeout: 10000,
+        headers : {
+            'X-SPIN-API-ACCESS-TOKEN' : spiniAPIAccessToken
+        }
+    });
+
+    restlerPost.on('complete', function(data, response) {
+        logger.info("call to backend API was completed for leadgen : " + leadgenID + " with status code : "+ response.statusCode.toString());
+        logger.info("response headers : " + JSON.stringify(response.headers));
+        logger.info("response payload : " + JSON.stringify(data));
+    });
+
+    restlerPost.on('timeout', function() {
+        logger.error("timed out in calling the backend API : " + spiniBackendAPIEndpoint + "  for trackingID : " + leadgenID);
+    });
+}
